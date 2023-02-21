0df76387e5ad63dd4c5d7eae411b712a1a53d703
Alex Warda <alexsio03@gmail.com>
2023-02-21 12:54:15 -0800

    Formatted for requirements, changed conversion to only PLN, edited output text

--- index.js
+++ index.js
@@ -1,59 +1,47 @@
-console.log("accessed");
 document.addEventListener("DOMContentLoaded", function() {
-    // Define the initial conversion to be performed
-    const fromCurrency = "PLN";
-    const toCurrency = "GBP";
-    let amount = 100;
-  
-    // Call the convertCurrency() function to perform the initial conversion
+  // Define the initial conversion to be performed
+  let fromCurrency = "GBP";
+  let toCurrency = "PLN";
+  let amount = 20;
+
+  // Call the convertCurrency() function to perform the initial conversion
+  convertCurrency(fromCurrency, toCurrency, amount);
+
+  // Attach event listeners to the input fields to update the conversion result live
+  let amountField = document.getElementById("amount");
+  let fromCurrencyField = document.getElementById("from-currency");
+
+  amountField.addEventListener("input", function() {
+    amount = amountField.value;
     convertCurrency(fromCurrency, toCurrency, amount);
-  
-    // Add an event listener to the input field to update the conversion on change
-    const amountInput = document.getElementById("amount");
-    amountInput.addEventListener("input", function() {
-      amount = amountInput.value;
-      convertCurrency(fromCurrency, toCurrency, amount);
-    });
-  
-    // Add an event listener to the select fields to update the conversion on change
-    const fromCurrencySelect = document.getElementById("from-currency");
-    const toCurrencySelect = document.getElementById("to-currency");
-    fromCurrencySelect.addEventListener("change", function() {
-      fromCurrency = fromCurrencySelect.value;
-      convertCurrency(fromCurrency, toCurrency, amount);
-    });
-    toCurrencySelect.addEventListener("change", function() {
-      toCurrency = toCurrencySelect.value;
-      convertCurrency(fromCurrency, toCurrency, amount);
-    });
   });
+
+  fromCurrencyField.addEventListener("change", function() {
+    fromCurrency = fromCurrencyField.value;
+    convertCurrency(fromCurrency, toCurrency, amount);
+  });
+});
+
   
-  function convertCurrency(fromCurrency, toCurrency, amount) {
-    const apiUrl = `https://api.nbp.pl/api/exchangerates/rates/a/${toCurrency}/?format=json`;
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        const exchangeRate = data.rates[0].mid;
-        let resultAmount;
-        if (fromCurrency === "PLN") {
-          resultAmount = amount / exchangeRate;
-          const result = `${amount} PLN is ${resultAmount.toFixed(2)} ${toCurrency} at the current exchange rate of ${exchangeRate}.`;
-          displayResult(result);
-        } else if (toCurrency === "PLN") {
-          resultAmount = amount * exchangeRate;
-          const result = `${amount} ${fromCurrency} is ${resultAmount.toFixed(2)} PLN at the current exchange rate of ${exchangeRate}.`;
-          displayResult(result);
-        } else {
-          const plnAmount = amount / exchangeRate;
-          const resultAmount = plnAmount * exchangeRate;
-          const result = `${amount} ${fromCurrency} is ${resultAmount.toFixed(2)} ${toCurrency} at the current exchange rate of ${exchangeRate}.`;
-          displayResult(result);
-        }
-      })
-      .catch(error => console.error(error));
-  }
-  
-  function displayResult(result) {
-    const resultElement = document.getElementById("result");
-    resultElement.textContent = result;
-  }
+
+function convertCurrency(fromCurrency, toCurrency, amount) {
+  // Fetch the exchange rate from the API using the provided URL
+  fetch(`https://api.nbp.pl/api/exchangerates/rates/a/${fromCurrency}/?format=json&callback=callback`)
+    .then(response => response.json())
+    .then(data => {
+      let exchangeRate = data.rates[0].mid;
+      let resultAmount = amount * exchangeRate;
+      let result = `${amount} ${fromCurrency} = ${resultAmount.toFixed(2)} ${toCurrency}`;
+      displayResult(result);
+    })
+    .catch(error => {
+      console.error(error);
+      let result = "An error occurred while fetching the exchange rate. Please try again later.";
+      displayResult(result);
+    });
+}
+
+function displayResult(result) {
+  const resultElement = document.getElementById("result");
+  resultElement.textContent = result;
+}
