aff764ceb37c06c57f1d2ce0a0e80df061e9e3e9
Alex Warda <alexsio03@gmail.com>
2023-02-24 09:33:07 -0800

    Added documentation

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Wait for the DOM to be fully loaded before running the script
 document.addEventListener("DOMContentLoaded", function() {
   // Define the initial conversion to be performed
   let fromCurrency = "GBP";
@@ -11,24 +12,28 @@ document.addEventListener("DOMContentLoaded", function() {
   let fromCurrencyField = document.getElementById("from-currency");
   let toAmountField = document.getElementById("to-amount");
   
-
+  // Update the result when the user changes the 'from' amount
   fromAmountField.addEventListener("input", function() {
     amount = fromAmountField.value;
     convertCurrency(fromCurrency, 0, amount);
   });
 
+  // Update the result when the user changes the 'from' currency
   fromCurrencyField.addEventListener("change", function() {
     fromCurrency = fromCurrencyField.value;
     convertCurrency(fromCurrency, 0, amount);
   });
 
+  // Update the 'from' amount when the user changes the 'to' amount
   toAmountField.addEventListener("change", function() {
     amount = toAmountField.value;
     convertCurrency(fromCurrency, 1, amount);
   })
 });
 
+// Fetch the exchange rate and update the result
 function convertCurrency(fromCurrency, reverse, amount) {
+  // Set the flag icon based on the 'from' currency
   let flag = document.getElementById("flag"); 
   if(fromCurrency == "GBP") {
     flag.innerHTML = `<i class="flag flag-united-kingdom"></i>`;
@@ -37,14 +42,22 @@ function convertCurrency(fromCurrency, reverse, amount) {
   } else {
     flag.innerHTML = `<i class="flag flag-eu"></i>`;
   }
+
+  // Get the element that displays the result
   const resultElement = document.getElementById("result");
+
   // Fetch the exchange rate from the API using the provided URL
   fetch(`https://api.nbp.pl/api/exchangerates/rates/a/${fromCurrency}/?format=json&callback=callback`)
     .then(response => response.json())
     .then(data => {
+      // Extract the exchange rate from the API response
       let exchangeRate = data.rates[0].mid;
+
+      // Display the exchange rate in the result element
       let result = `1 ${fromCurrency} = <b>${exchangeRate.toFixed(2)} PLN</b>`;
       resultElement.innerHTML = result;
+
+      // Convert the amount based on the whether it is 'to' or 'from'
       if(reverse) {
         let resultAmount = amount / exchangeRate;
         displayFrom(resultAmount);
@@ -54,17 +67,20 @@ function convertCurrency(fromCurrency, reverse, amount) {
       }
     })
     .catch(error => {
+      // Display an error message if the API request fails
       console.error(error);
       let result = "An error occurred while fetching the exchange rate. Please try again later.";
       resultElement.innerHTML = result;
     });
 }
 
+// Update the 'to' amount in the UI
 function displayTo(resultAmount) {
   const toAmount = document.getElementById("to-amount");
   toAmount.value = resultAmount.toFixed(2);
 }
 
+// Update the 'from' amount in the UI
 function displayFrom(resultAmount) {
   const fromAmount = document.getElementById("from-amount");
   fromAmount.value = resultAmount.toFixed(2);
